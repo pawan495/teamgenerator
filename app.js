75a0c2d20f5fabc300256d05f6d59c0bc3ffe386
pawan495 <pawanpreetkaur495@gmail.com>
2022-03-22 13:01:42 -0400

    add js

---
+++ app.js
@@ -0,0 +1,5 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const makeEmployee = require("./lib/makeEmployee");
+const inquirer = require("inquirer");
